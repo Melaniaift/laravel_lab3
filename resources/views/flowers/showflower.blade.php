22baeaf56b847295ff3ad8353c37da076635c218
Melaniaift <ioanamelany99@yahoo.com>
2021-07-06 16:02:59 +0300

    laravellab3

---
+++ resources/views/flowers/showflower.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head><title>Flowers</title></head>
+    <body>
+        <h1>
+            Details for <?php echo $flower->nume;?>
+        </h1>
+        <h3>
+            Culoare: <?php echo $flower->culoare;?>
+        </h3>
+        <h3>
+            Marime: <?php echo $flower->marime;?>
+        </h3>
+        <h3>
+            Pret: <?php echo $flower->pret;?>
+        </h3>
+        <a href='http://localhost/LaravelLab3/public'>Back</a>
+    </body>
+
+</html>
